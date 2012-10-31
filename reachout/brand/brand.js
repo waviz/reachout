5f78ed4d3c9d34a284a81ebffce3ac6c2b794a16
Maneesh Kumar <Maneesh.Kumar@contractor.cengage.com>
2012-10-31 19:24:39 +0530

    Added Structure

--- reachout/brand/brand.js
+++ reachout/brand/brand.js
@@ -1,17 +1,13 @@
-steal( 'jquery/controller',
-       'jquery/view/ejs',
-	   'jquery/dom/form_params',
-	   'jquery/controller/view',
-	   'reachout/models' )
-	.then('./views/init.ejs', function($){
-
+steal('reachout/css/reachout.css', 'reachout/models/models.js','reachout/fixtures/fixtures.js')
+.then('jquery/controller', 'jquery/view/ejs','jquery/dom/form_params', 'jquery/controller/view', 'reachout/models', './views/init.ejs')
+.then( function($){
 /**
  * @class reachout.Brand
  * @parent index
  * @inherits jQuery.Controller
  * Creates brands
  */
-$.Controller('reachout.Brand',
+$.Controller('Reachout.Brand',
 /** @Prototype */
 {
 	init : function(){
@@ -19,5 +15,8 @@ $.Controller('reachout.Brand',
 	}
 	
 });
+$(document).ready(function(){
+    $("#homepageGridDynamic").reachout_brand();
+}); 
 
 });
